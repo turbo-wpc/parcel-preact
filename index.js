8c63107e116912f71ee756dd70e61057c2516827
wangpengchong <wangpengchong@autohome.com.cn>
2018-11-26 17:51:04 +0800

    test

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 import './src/css/index'
+import { h, render, Component } from 'preact'
+
+class App extends Component {
+    componentDidMount(){
+        console.log(666)
+    }
+    render() {
+        let time = new Date().toLocaleTimeString()
+        return (
+            <div>
+                <span>{ time }</span>
+            </div>
+        )
+    }
+}
+
+render(<App />, document.getElementById('root'))
